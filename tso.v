c003e0eaaf10bae94e96e7830f9f12b4a1b7caa3
Xuankang Lin <lxk19921205@gmail.com>
2014-04-09 22:41:34 -0400

    add new file and comments

---
+++ tso.v
@@ -0,0 +1,17 @@
+(**
+
+This is for the final project of CS565 Programming Languages in
+Purdue.
+
+Write a simple programming language that exposes relaxed memory
+semantics (e.g. TSO). Prove that data race free programs have SC
+semantics.
+
+Written in Coq, mechanically formalized and verified.
+
+Andriy LIN
+
+Updated: 04/09/2014
+
+*)
+
